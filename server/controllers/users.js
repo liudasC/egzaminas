e902886ece9cdfc484d6b9f4b9bf2b2bcd10c6b9
liudasC <l.cekavicius@gmail.com>
2021-03-31 11:01:59 +0300

    feat: created - update, delete controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,12 +4,12 @@ const UserModel = require("../models/UsersModel");
 // GET
 const getUsers = async (req, res) => {
     try {
-const users = await UserModel.find();
-res.status(200).json(users.map(({ _id, name, age, email, password}) =>
-({ id: _id, name, age, email, password})
-))
+        const users = await UserModel.find();
+        res.status(200).json(users.map(({ _id, name, age, email, password }) =>
+            ({ id: _id, name, age, email, password })
+        ))
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json({ ERROR: error.message })
     }
 }
 
@@ -26,16 +26,35 @@ const createUser = async (req, res) => {
             password: createdUser.password,
         })
     } catch (error) {
-        res.status(404).json(error)
+        res.status(404).json({ ERROR: error.message })
     }
 }
 
+// UPDATE
 const updateUser = async (req, res) => {
-
+    const { id } = req.params;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id))
+        throw new Error(`Neteisingas id '${id}' formatas`)
+const updatedUser = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+res.status(200).json({updatedUser})
+    } catch (error) {
+        res.status(404).json({ ERROR: error.message })
+    }
 }
 
+// DELETE
 const deleteUser = async (req, res) => {
-
+    const { id } = req.params;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id))
+            throw new Error(`Neteisingas id '${id}' formatas`)
+        const deletedUser = await UserModel.findByIdAndDelete(id);
+        if (!deletedUser) throw new Error("failed to delete");
+        res.status(200).json({ message: "Deleted successfully" })
+    } catch (error) {
+        res.status(404).json({ ERROR: error.message })
+    }
 }
 
 module.exports = {
