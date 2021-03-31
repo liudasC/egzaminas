9011cecf0fffcf867cbd9a8268502716bd4b4447
liudasC <l.cekavicius@gmail.com>
2021-03-31 11:45:06 +0300

    feat: created - React app, API

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+// import API from "./api"
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/api/index.js
@@ -0,0 +1,77 @@
+const requestOptions = {
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json' },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+};
+
+// susiet portą su .env nepavyko
+const domain = `http://localhost:5000`;
+
+export const getUsers = async (success, failure) => {
+    try {
+        const response = await fetch(`${domain}/users`, requestOptions)
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message)
+        success(data);
+    } catch (error) {
+        failure(error.message)
+    }
+}
+
+export const postUser = async (body, success, failure) => {
+    try {
+        const response = await fetch(`${domain}/users`, {
+            ...requestOptions,
+            method: 'POST',
+            body: JSON.stringify(body),
+          })
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message)
+        success(data);
+    } catch (error) {
+        failure(error.message)
+    }
+}
+
+export const updateUser = async (id, body, success, failure) => {
+    try {
+        const response = await fetch(`${domain}/users/${id}`, {
+            ...requestOptions,
+            method: 'PATCH',
+            body: JSON.stringify(body),
+          })
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message)
+        success(data);
+    } catch (error) {
+        failure(error.message)
+    }
+}
+
+export const deleteUser = async (id, success, failure) => {
+    try {
+        const response = await fetch(`${domain}/users/${id}`, {
+            ...requestOptions,
+            method: 'DELETE',
+          })
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message)
+        success(data);
+    } catch (error) {
+        failure(error.message)
+    }
+}
+
+
+
+const API = {
+    getUsers,
+    postUser,
+    updateUser,
+    deleteUser
+};
+
+export default API;
